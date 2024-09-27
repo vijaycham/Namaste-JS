f0462df7f3ee47d9adfd009412cc221b88c4c662
vijaycham <104141481+vijaycham@users.noreply.github.com>
2024-09-27 06:07:15 +0530

    Hositing revision

---
+++ App.js
@@ -0,0 +1,13 @@
+
+
+function getname() {
+  console.log("Hai eveyone");
+
+}
+console.log(getname)
+getname();
+console.log(x);
+
+var x = 7;
+
+console.log(x);
---
+++ read.md
@@ -0,0 +1,4 @@
+Hositing -
+    Accessing the variables and functions before their declaration or initailtization is called Hositing 
+
+![alt text](image.png)
